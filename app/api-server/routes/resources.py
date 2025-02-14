c1db4ad4737a00f6a1c86f4f3e5a08864418ee8e
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-14 10:10:10 +0200

    Add get_resources, add docstrings

--- app/api-server/routes/resources.py
+++ app/api-server/routes/resources.py
@@ -4,4 +4,15 @@ router = APIRouter()
 
 @router.post("/")
 def request_resources():
-    pass
+    """
+    Request for provisioning of resources (CU, DU)
+    sends connection details for the core-CU connection as a
+    response if successfull
+    """
+
+@router.get("/")
+def get_resources():
+    """
+    Returns the resources currently in use by the client
+    and the connection details for those resources.
+    """
