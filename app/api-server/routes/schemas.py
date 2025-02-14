b2f09af1241535c358cda6b13b63d152c08b122b
Emil Kelhälä <emil.kelhala@gmail.com>
2025-02-14 10:14:40 +0200

    Add initial parameters to ResourceRequest

--- app/api-server/routes/schemas.py
+++ app/api-server/routes/schemas.py
@@ -5,4 +5,6 @@ class ResourceRequest(BaseModel):
     A schema representing the data needed for
     provisioning new resources
     """
-    
+    num_rus: int # number of RUs to connect to
+    core_ip: str # AMF IP address of the core network for CU
+    core_port: int # AMF port of the core for CU to connect to
