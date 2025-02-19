b26cf7a7b18ca94d3557ab7cb63314927912d0fd
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-02-19 09:32:35 +0200

    Add initial consume logic to main.py

---
+++ app/worker/main.py
@@ -0,0 +1,24 @@
+import os
+import pika
+from dotenv import load_dotenv
+load_dotenv()
+
+connection_credentials = pika.PlainCredentials(username=os.getenv("RABBITMQ_USERNAME"),
+                                                password=os.getenv("RABBITMQ_PASSWORD"))
+connection_params = pika.ConnectionParameters(
+                                                host=os.getenv("RABBITMQ_BROKER"),
+                                                port=os.getenv("RABBITMQ_PORT"),
+                                                credentials=connection_credentials)
+
+connection = pika.BlockingConnection()
+channel = connection.channel()
+channel.queue_declare("work-queue")
+
+def work_callback(ch, method, properties, body):
+    print(f"received {body}")
+
+channel.basic_consume(queue="work-queue",
+                        on_message_callback=work_callback,
+                        auto_ack=True)
+
+channel.start_consuming()
