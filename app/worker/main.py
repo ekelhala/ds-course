b4861452364af93229e23b54a3a06f4158033702
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-03-04 09:10:26 +0200

    Add more debug prints, update service names in deployment

--- app/worker/main.py
+++ app/worker/main.py
@@ -25,10 +25,11 @@ channel = connection.channel()
 channel.queue_declare("work-queue")
 
 def work_callback(ch, method, properties, body):
+    print("message received")
     resource_details = json.loads(body)
     config_id = str(uuid.uuid4())
-    cu_config = helpers.make_cu_configmap(resource_details.core_ip,
-                                         resource_details.core_port,
+    cu_config = helpers.make_cu_configmap(resource_details["core_ip"],
+                                         resource_details["core_port"],
                                          config_id)
     kubernetes_client.create_namespaced_config_map(namespace="default", body=cu_config)
     deployment_config = helpers.make_deployment_config(config_id)
@@ -44,3 +45,4 @@ channel.basic_consume(queue="work-queue",
                         auto_ack=True)
 
 channel.start_consuming()
+print("worker started")
