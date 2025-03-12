e2ea671c1869ebeb73899fd5bcdd6a2f4b33a586
Emil Kelhälä <emil.kelhala@protonmail.com>
2025-03-12 11:19:55 +0200

    Add logging for MongoDB connection status

--- app/api_server/app.py
+++ app/api_server/app.py
@@ -1,16 +1,24 @@
 import json
 import os
+import logging
 from flask import Flask
 from werkzeug.exceptions import HTTPException
-from mongoengine import connect
+from mongoengine import connect, ConnectionFailure
 from prometheus_flask_exporter import PrometheusMetrics
 from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
 PrometheusMetrics(app)
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 
-connect(host=os.getenv("MONGODB_URI"), name="db")
+try:
+    connect(host=os.getenv("MONGODB_URI"), name="db")
+    logger.info("Connected to MongoDB at %s", os.getenv("MONGODB_URI"))
+except ConnectionFailure:
+    logger.error("Connection to MongoDB failed")
 
 from api_server.api import api_bp
 from api_server.utils import RANResourceConverter
